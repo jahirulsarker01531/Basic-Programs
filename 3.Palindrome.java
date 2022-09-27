0820da034c4b0e0398bd0042748f2dfd66eeb1bc
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-09-27 21:24:10 +0600

    Create 3.Palindrome.java

---
+++ 3.Palindrome.java
@@ -0,0 +1,18 @@
+//palindrome
+public class Simple{
+    public static void main(String[] args){
+        int n = 150, sum = 0, r;
+        int count = n;
+        while(n>0){
+            r = n%10;
+            sum = r + (sum*10);
+            n = n/10;
+        }
+        //System.out.println("Sum is: "+sum);
+        if(count == sum){
+            System.out.println("palindrome");
+        }else{
+            System.out.println("not palindrome");
+        }
+    }
+}
