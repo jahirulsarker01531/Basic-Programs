8903f3306d701b2263f9a08f8937c4a822b21111
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-09-28 10:40:51 +0600

    Update 3.Palindrome.java

--- 3.Palindrome.java
+++ 3.Palindrome.java
@@ -17,4 +17,28 @@ public class Simple{
     }
 }
 
-//
+// using string, efficient, can't be use for anything
+
+import java.util.*;
+public class Simple{
+    public static void main(String[] args){
+        String original = "";
+        String reverse = "";
+        Scanner x = new Scanner(System.in);
+        System.out.println("Enter a string/number to check if it is a palindrome");
+        original = x.nextLine();
+
+        int length = original.length();
+
+        for(int i=length-1; i>=0; i--){
+            reverse = reverse + original.charAt(i);
+        }
+
+        if(original.equals(reverse)){
+            System.out.println("Entered string/number is a palindrome.");    
+        }else{
+            System.out.println("Entered string/number isn't a palindrome.");
+        }
+    }
+}
+
