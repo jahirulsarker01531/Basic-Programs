28b99306ae8fea19fd6b618c7308e8d6762a0b8c
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-09-28 14:12:43 +0600

    Create 6.Random_generating.java

---
+++ 6.Random_generating.java
@@ -0,0 +1,13 @@
+import java.lang.Math;
+public class Simple{
+    public static void main(String[] args){
+        System.out.println("random1: "+Math.random());
+        System.out.println("random2: "+Math.random());
+        System.out.println("random3: "+Math.random());
+        System.out.println("random4: "+Math.random());
+        System.out.println("random5: "+Math.random());
+        System.out.println("random6: "+Math.random());
+        System.out.println("random7: "+Math.random());
+    }
+}
+
