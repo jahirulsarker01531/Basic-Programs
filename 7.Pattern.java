033986f850ecd4e9e125674434140f0ed063a53e
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-06 03:04:43 +0600

    Create 7.Pattern.java

---
+++ 7.Pattern.java
@@ -0,0 +1,27 @@
+//Right triangle star pattern
+public class Simple{
+    public static void main(String[] args){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+//Left triangle star pattern
+public class Simple{
+    public static void main(String[] args){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<(10-i-1)*2; j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
