a701ee6447ca8e26ad96a8076a9b6293bc045609
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-06 03:44:47 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -25,3 +25,18 @@ public class Simple{
         }
     }
 }
+
+//Pyramid Star Pattern
+public class Simple{
+    public static void main(String[] args){
+        for(int i=0; i<10; i++){
+            for(int j=0; j<(10-i); j++){
+                System.out.print(" ");
+            }
+            for(int j=0; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
