db5bc48960844e2b81a9341225429c35c91f10f8
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-05 18:27:03 +0600

    Update 6.Random_generating.java

--- 6.Random_generating.java
+++ 6.Random_generating.java
@@ -104,3 +104,22 @@ public class Simple{
 
     }
 }
+
+//Random Number Generation in Java 8
+import java.util.Random;
+public class Simple{
+    public static void main(String[] args){
+        randomInts(5);
+        randomInts(9, 50, 90);
+    }
+    //method that generates a stream of integers having size 5
+    public static void randomInts(int num){
+        Random random = new Random();
+        random.ints(num).forEach(System.out::println);
+    }
+    //method that generates a stream of 9 integers between 50 to 90
+    public static void randomInts(int num, int origin, int bound){
+        Random random1 = new Random();
+        random1.ints(num, origin, bound).forEach(System.out::println);
+    }
+}
