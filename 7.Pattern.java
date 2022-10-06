d676e2e45fcc9aac240ab90d74eda79ddf27898a
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-06 18:48:43 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -110,3 +110,23 @@ public class Simple{
         }
     }
 }
+
+//Reverse Pyramid Star Pattern
+import java.util.Scanner;
+public class Simple{
+    public static void main(String[] args){
+        System.out.println("Enter the number of rows: ");
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=row-i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
