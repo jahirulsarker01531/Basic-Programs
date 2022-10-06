fb57ff905bb8e6cd9568e81c1eba3f1bcf8ec3dd
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-06 17:16:40 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -72,5 +72,22 @@ public class Simple{
     }
 }
 
-
+//downward triangle star pattern
+import java.util.Scanner;
+public class Simple{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
+        int row = sc.nextInt();
+        for(int i=1; i<=row; i++){
+            for(int j=i+1; j<=row; j++){
+                System.out.print("* ");
+            }
+            for(int j=1; j<=2*(i+1); j++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
     
