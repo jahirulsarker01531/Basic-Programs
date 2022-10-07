7501cf0f7dc16b5cd5e4656a30d685db1780a2e3
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-07 14:50:30 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -130,3 +130,48 @@ public class Simple{
         }
     }
 }
+
+//Right Down Mirror Pattern
+import java.util.Scanner;
+public class Simple{
+    public static void main(String[] args){
+        System.out.println("Enter the number of rows:");
+        Scanner sc = new Scanner(System.in);
+        int row = sc.nextInt();
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=row-i+1; j++){
+                System.out.print("*");
+            }
+           
+            System.out.println();
+        }
+    }
+}
+
+
+//Right Pascal's Triangle
+import java.util.Scanner;
+public class Simple{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows you want to print: ");
+        int row = sc.nextInt();
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for(int i=1; i<row; i++){
+            for(int j=1; j<=(row-i); j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }        
+    }
+}
