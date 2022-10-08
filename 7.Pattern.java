ad80ade60b1a03530d80c4a7eaa58143ea4eb34d
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-08 11:23:25 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -204,3 +204,33 @@ public class Simple{
 
     }
 }
+
+//Sandglass Star Pattern
+import java.util.Scanner;
+public class Simple{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows you want to print: ");
+        int row = sc.nextInt();
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=row-i+1; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i=1; i<=row; i++){
+            for(int j=1; j<=row-i+1; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
