38cbf86218efe71e24c808da8ff38de5acd59a1d
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-09 13:57:31 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -234,3 +234,18 @@ public class Simple{
         }
     }
 }
+
+//Alphabet Star Pattern
+public class Simple{
+    public static void main(String[] args){
+        int i, j, n=8;
+        for(i=0; i<=n; i++){
+            for(j=0; j<=n/2; j++){
+                if((i!=0 &&(j==0||j==n/2||i==n/2)) || (j!=n/2 && (j==0||i==0||i==n/2))){
+                    System.out.print("*");
+                }else System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
