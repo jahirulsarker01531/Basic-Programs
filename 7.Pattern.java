9305477cda2c5766a464ed8199a73ef920918bf8
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-15 22:36:36 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -249,3 +249,26 @@ public class Simple{
         }
     }
 }
+
+//Triangle star pattern
+import java.util.Scanner;
+public class Simple{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int row = sc.nextInt();
+        for(int i=1; i<=row; i++){
+            for(int j=i; j<=row; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<2*i; j++){
+                if(j==1||i==row||j==(2*i-1)){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
