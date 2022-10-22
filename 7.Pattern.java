7ea7ed87129d9fb3ba58a265b0c66865faec15d6
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-10-22 22:11:32 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -272,3 +272,26 @@ public class Simple{
         }
     }
 }
+
+
+//Down Triangle Pattern
+import java.util.Scanner;
+public class Simple{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        for(int i=1; i<=rows; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(" ");
+            }
+            for(int j=1; j<=((rows-i+1)*2-1); j++){
+                if(i==1||j==((rows-i+1)*2-1)||i==rows||j==1){
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
