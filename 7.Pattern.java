bb89502f2809acfca7d5146c78858042c4babd9e
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2022-11-15 21:48:55 +0600

    Update 7.Pattern.java

--- 7.Pattern.java
+++ 7.Pattern.java
@@ -295,3 +295,38 @@ public class Simple{
         }
     }
 }
+
+//Diamond Star Pattern
+import java.util.Scanner;
+public class Simple{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of rows you want: ");
+        int rows = sc.nextInt();
+        
+        for(int i=1; i<=rows; i++){
+            for(int j=rows; j>i; j--){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for(int k=1; k<2*(i-1); k++){
+                System.out.print(" ");
+            }
+            if(i==1){
+                System.out.println("");
+            }else System.out.println("*");
+        }
+        for(int i=rows-1; i>=1; i--){
+            for(int j=rows; j>i; j--){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            for(int k=1; k<2*(i-1); k++){
+                System.out.print(" ");
+            }
+            if(i==1){
+                System.out.println("");
+            }else System.out.println("*");
+        }
+    }
+}
