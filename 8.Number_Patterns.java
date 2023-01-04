2904a283a591f634da97a3e87fe04dafae908b22
Jahirul Sarker <92864749+jahirulsarker01531@users.noreply.github.com>
2023-01-04 20:05:24 +0600

    Update 8.Number_Patterns.java

--- 8.Number_Patterns.java
+++ 8.Number_Patterns.java
@@ -14,3 +14,18 @@ public class Main{
 		}
 	}
 }
+
+//2.Pattern2
+package pac1;
+
+public class Main{
+	public static void main(String[] args) {
+		int x = 0;
+		for(int i=1; i<=7; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print(++x + " ");
+			}
+			System.out.println();
+		}
+	}
+}
